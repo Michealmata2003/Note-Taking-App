aa1b374a16304339973380666964592eb3a8003f
Feranmi <omotoshoferanmi123@gmail.com>
2025-10-21 13:58:31 -0700

    I created the new pages and the dropdown for notebooks

--- frontend/src/components/dashboard/Binmain.jsx
+++ frontend/src/components/dashboard/Binmain.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 import { dummyNotes } from "./DummyNotes";
 import { noteColors } from "../../utils/Color";
 
-function Remindermain() {
+function Binmain() {
   const getRandomColor = () => {
     const randomIndex = Math.floor(Math.random() * noteColors.length);
     return noteColors[randomIndex];
@@ -13,41 +13,35 @@ function Remindermain() {
     <main className="pt-20 p-4 max-w-1xl mx-auto w-full space-y-4">
         <section className="flex justify-between items-center w-full px-3 py-3">
           <div className="flex justify-between items-stretch w-full space-x-7">
-            <form className="flex-1">
-              <input
-                      type="text"
-                      placeholder="Note"
-                      className="p-5 w-full border rounded-lg outline-none"
-                    />
-            </form>
-            <div className="text-4xl bg-white w-20 p-4 flex items-center justify-between space-x-1 border rounded-lg">
-                    <MdOutlineMailOutline className="" />
+            
+                <div className="border rounded p-6 flex justify-between items-center w-full">
+                    <div className="text-2xl">
+                            <h1>Your Deleted Notes</h1>
+                    </div>
+                    <button className="border border-gray-500 pr-3 pl-3 pt-2 pb-2 rounded-lg text-gray-800 hover:bg-black hover:text-white transition">
+                        Empty Bin
+                    </button>
+
+                </div>
+
+               <div className="hidden md:flex text-4xl bg-white w-20 p-4 items-center justify-between space-x-1 border rounded-lg">
+                    <MdOutlineMailOutline />
                     <IoMdNotificationsOutline />
-                  </div>
+                </div>
             </div>
+
         </section>
 
         <section className="bg-white border rounded-lg p-1">
           <div className="p-3">
-            <h1 className="text-3xl">Your Reminder Notes</h1>
-
-            {/* Form Section */}
-            <div className="mt-3 w-full">
-              <form className="w-full">
-                <input
-                  type="text"
-                  placeholder="Note"
-                  className="p-2 w-full border rounded-lg outline-none"
-                />
-              </form>
-            </div>
+            
 
             {/* Cards Section */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-7">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 ">
               {dummyNotes.slice(0, 6).map((notes) => (
                 <div
                   key={notes.id}
-                  className={`p-6 space-y-4 border rounded-2xl shadow-sm ${getRandomColor()} hover:shadow-md transition-all duration-300 cursor-pointer`}
+                  className={`p-6 space-y-4 rounded-2xl shadow-sm ${getRandomColor()} hover:shadow-md transition-all duration-300 cursor-pointer`}
                 >
                   <div className="flex justify-between items-center">
                     <MdOutlineMailOutline className="text-4xl p-2 border-1 border-blue-500 rounded-lg" />
@@ -81,4 +75,4 @@ function Remindermain() {
   )
 }
 
-export default Remindermain
+export default Binmain
